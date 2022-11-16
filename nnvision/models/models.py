036b3c1aff1e5e49b30a53b47a0e9f886e20403e
Santiago Cadena <sa.cadena721@gmail.com>
2022-11-16 13:52:40 -0600

    remove unused dependency

--- nnvision/models/models.py
+++ nnvision/models/models.py
@@ -8,7 +8,7 @@ from mlutils.layers.readouts import PointPooled2d, FullGaussian2d
 from mlutils.layers.activations import MultiplePiecewiseLinearExpNonlinearity
 
 from nnfabrik.builder import get_model
-from nnfabrik.utility.nn_helpers import get_module_output, set_random_seed, get_dims_for_loader_dict
+from nnfabrik.utility.nn_helpers import set_random_seed, get_dims_for_loader_dict
 from torch import nn
 from torch.nn import functional as F
 
